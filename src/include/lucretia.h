c4dad2d65782ef5ad018dcdbecbb24b3a11c5e9f
clfv5 <bedirhanakcay@icloud.com>
2021-11-30 22:02:01 +0300

    Lucretia server header file created:

---
+++ src/include/lucretia.h
@@ -0,0 +1,38 @@
+#ifndef __LUCRETIA_H_
+#define __LUCRETIA_H_
+
+#include "map.h"
+
+#include <sys/types.h>
+#include <netinet/in.h>
+
+#define LUCRETIA_ERROR_PROPS_NULL -1
+#define LUCRETIA_ERROR_MEM_ALLOC -2
+
+/*
+ * Configuration types of a Lucretia server.
+ */
+enum conf_type
+{
+    MASTER,
+    SLAVE
+};
+
+struct l_in_addr
+{
+    int16_t PORT;
+    in_addr_t addr;
+};
+
+struct lucretia
+{
+    enum conf_type type;
+
+    struct l_in_addr in_addr;
+
+    struct map *properties;
+};
+
+struct lucretia *new_lucretia(struct map *props);
+
+#endif // !__LUCRETIA_H_

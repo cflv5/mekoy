d2f99add56c4720e7d53c4f3e4b45e1e329860d7
clfv5 <bedirhanakcay@icloud.com>
2021-12-03 10:34:50 +0300

    refactored to meet new deserialize function

--- test/lcp.test.c
+++ test/lcp.test.c
@@ -58,6 +58,7 @@ int serialize_lcp_req__given_lcp_req()
 int deserialize_lcp_req__given_lcp_req()
 {
     int pos = 1;
+    int size;
     u_int16_t *p_opcode = NULL;
 
     char message[BUFF_LEN];
@@ -66,7 +67,7 @@ int deserialize_lcp_req__given_lcp_req()
     char str_req[BUFF_LEN];
     memset((void *)str_req, 0, BUFF_LEN);
 
-    struct lcp_req req;
+    struct lcp_req *req;
 
     message[0] = 1;
 
@@ -88,10 +89,11 @@ int deserialize_lcp_req__given_lcp_req()
     strncpy(&message[pos], "body", 6);
     pos += 5;
 
-    if (deserialize_lcp_req(&req, message, BUFF_LEN) < 0)
+    req = deserialize_lcp_req(message, BUFF_LEN, &size);
+    if (req < 0)
         return 0;
 
-    to_str_lcp_req(&req, str_req, BUFF_LEN);
+    to_str_lcp_req(req, str_req, BUFF_LEN);
     fprintf(stdout, "\nDeserialized req:\n%s", str_req);
 
     return 1;

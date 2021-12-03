9574ac7264f87612ba208623b04e698457ea4f95
clfv5 <bedirhanakcay@icloud.com>
2021-12-03 11:02:53 +0300

    implementation of lcp handhake

--- src/include/lucretia.h
+++ src/include/lucretia.h
@@ -8,6 +8,14 @@
 
 #define LUCRETIA_ERROR_PROPS_NULL -1
 #define LUCRETIA_ERROR_MEM_ALLOC -2
+#define LUCRETIA_ERROR_NON_MASTER_OPERATION -3
+#define LUCRETIA_ERROR_SOCKET_CREATION -4
+#define LUCRETIA_ERROR_CONNECTION -5
+#define LUCRETIA_ERROR_REQUEST_SERIALIZATION -6
+#define LUCRETIA_ERROR_SEND_OPERATION -7
+#define LUCRETIA_ERROR_CONNECTION_SHUTDOWN -8
+#define LUCRETIA_ERROR_HANDSHAKE_MISMACHED_RESPONSE_VALUES -9
+#define LUCRETIA_ERROR_HANDSHAKE_MASTER_REJECTS -10
 
 /*
  * Configuration types of a Lucretia server.
@@ -52,5 +60,6 @@ struct lucretia
 };
 
 struct lucretia *new_lucretia(struct map *props);
+int lcp_handshake(struct lucretia *server, const char *address, in_port_t port)
 
 #endif // !__LUCRETIA_H_
--- src/lucretia.c
+++ src/lucretia.c
@@ -7,14 +7,24 @@
 
 #include "include/lucretia.h"
 #include "include/util.h"
+#include "include/lcp.h"
 
+#include <uuid/uuid.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <string.h>
+#include <stdio.h>
+
+#define BUFF_LEN 1024
 
 static enum conf_type set_conf_type(enum conf_type type);
 static u_int16_t get_port(char *portstr);
 static int get_max_slave_amount(char *max_slavestr);
 static struct l_node **set_slave_arr(int max_amount);
+static void
+create_req(struct lcp_req *req, u_char ver, const char *id,
+           const char *msgid, u_int16_t opcode, const char *header, const char *body);
+static int send_req(int sockfd, struct lcp_req *req);
 
 struct lucretia *new_lucretia(struct map *props)
 {
@@ -77,6 +87,187 @@ struct lucretia *new_lucretia(struct map *props)
     }
 
     return lucretia;
+}
+
+int lcp_handshake(struct lucretia *server, const char *address, in_port_t port)
+{
+    int sockfd;
+    int nsend;
+    int nread;
+    int id_len;
+
+    char msgid[UUID_STR_LEN];
+    char buffer[BUFF_LEN];
+    int buff_len;
+
+    struct sockaddr_in master_addr;
+
+    struct lcp_req req;
+    struct lcp_req *resp;
+    int resp_size;
+
+    const char *hello = "CAN I BE YOUR SLAVE";
+    const char *OK = "OK";
+
+    if (server->type != SLAVE)
+    {
+        return LUCRETIA_ERROR_NON_MASTER_OPERATION;
+    }
+
+    // creating socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        return LUCRETIA_ERROR_SOCKET_CREATION;
+    }
+
+    master_addr.sin_family = AF_INET;
+    master_addr.sin_addr.s_addr = inet_addr(address);
+    master_addr.sin_port = htons(port);
+
+    if (connect(sockfd, (const struct sockaddr *)&master_addr, sizeof(master_addr)) < 0)
+    {
+        close(sockfd);
+        return LUCRETIA_ERROR_CONNECTION;
+    }
+
+    get_uuid(msgid, UUID_STR_LEN);
+
+    // starting handshake
+    create_req(&req, 1, NULL, msgid, L_OP_HANDSHAKE, NULL, hello);
+    nsend = send_req(sockfd, &req);
+    if (nsend < 0)
+    {
+        shutdown(sockfd, SHUT_RDWR);
+        return nsend;
+    }
+
+    // master's response for handshake request
+    nread = recv(sockfd, (void *)buffer, BUFF_LEN, 0);
+    if (nread < 0)
+    {
+        shutdown(sockfd, SHUT_RDWR);
+        return LUCRETIA_ERROR_CONNECTION_SHUTDOWN;
+    }
+
+    resp = deserialize_lcp_req(buffer, BUFF_LEN, resp_size);
+    if (resp->opcode != L_OP_HANDSHAKE || strncmp(msgid, resp->msgid, strlen(msgid)) != 0)
+    {
+        shutdown(sockfd, SHUT_RDWR);
+        return LUCRETIA_ERROR_HANDSHAKE_MISMACHED_RESPONSE_VALUES;
+    }
+
+    if (strncmp(OK, resp->body, strlen(OK)) != 0)
+    {
+        shutdown(sockfd, SHUT_RDWR);
+        return LUCRETIA_ERROR_HANDSHAKE_MASTER_REJECTS;
+    }
+
+    // master allowed, sending port
+    buff_len = snprintf(buffer, BUFF_LEN, "PORT=%hu", server->addr.sin_port);
+    create_req(&req, 1, NULL, msgid, L_OP_HANDSHAKE, NULL, buffer);
+    nsend = send_req(sockfd, &req);
+    if (nsend < 0)
+    {
+        shutdown(sockfd, SHUT_RDWR);
+        return nsend;
+    }
+
+    // waiting master's response with id
+    nread = recv(sockfd, (void *)buffer, BUFF_LEN, 0);
+    if (nread < 0)
+    {
+        shutdown(sockfd, SHUT_RDWR);
+        return LUCRETIA_ERROR_CONNECTION_SHUTDOWN;
+    }
+
+    destroy_lcp_req(resp);
+    resp = deserialize_lcp_req(buffer, BUFF_LEN, resp_size);
+    if (resp->opcode != L_OP_HANDSHAKE || strncmp(msgid, resp->msgid, strlen(msgid)) != 0)
+    {
+        shutdown(sockfd, SHUT_RDWR);
+        return LUCRETIA_ERROR_HANDSHAKE_MISMACHED_RESPONSE_VALUES;
+    }
+
+    id_len = strlen(resp->body);
+    if (id_len == 0)
+    {
+        shutdown(sockfd, SHUT_RDWR);
+        return LUCRETIA_ERROR_HANDSHAKE_MISMACHED_RESPONSE_VALUES;
+    }
+
+    // setting id appointed by master
+    server->id_by_master = (char *)malloc(UUID_STR_LEN * sizeof(char));
+    strncpy(server->id_by_master, resp->body, UUID_STR_LEN);
+
+    // returning id to master to ack.
+    create_req(&req, 1, NULL, msgid, L_OP_HANDSHAKE, NULL, server->id_by_master);
+    nsend = send_req(sockfd, &req);
+    if (nsend < 0)
+    {
+        shutdown(sockfd, SHUT_RDWR);
+        return nsend;
+    }
+
+    // waiting master's ack. message
+    nread = recv(sockfd, (void *)buffer, BUFF_LEN, 0);
+    if (nread < 0)
+    {
+        shutdown(sockfd, SHUT_RDWR);
+        return LUCRETIA_ERROR_CONNECTION_SHUTDOWN;
+    }
+
+    destroy_lcp_req(resp);
+    resp = deserialize_lcp_req(buffer, BUFF_LEN, resp_size);
+    if (resp->opcode != L_OP_HANDSHAKE || strncmp(msgid, resp->msgid, strlen(msgid)) != 0)
+    {
+        shutdown(sockfd, SHUT_RDWR);
+        free(server->id_by_master);
+        return LUCRETIA_ERROR_HANDSHAKE_MISMACHED_RESPONSE_VALUES;
+    }
+
+    if (strncmp(OK, resp->body, strlen(OK)) != 0)
+    {
+        shutdown(sockfd, SHUT_RDWR);
+        free(server->id_by_master);
+        return LUCRETIA_ERROR_HANDSHAKE_MASTER_REJECTS;
+    }
+
+    shutdown(sockfd, SHUT_RDWR);
+
+    return 0;
+}
+
+static void
+create_req(struct lcp_req *req, u_char ver, const char *id,
+           const char *msgid, u_int16_t opcode, const char *header, const char *body)
+{
+    req->ver = ver;
+    req->id = id;
+    req->msgid = msgid;
+    req->opcode = opcode;
+    req->header = header;
+    req->body = body;
+}
+
+static int send_req(int sockfd, struct lcp_req *req)
+{
+    char message[BUFF_LEN];
+    int nsend;
+
+    int msglen = serialize_lcp_req(req, message, BUFF_LEN);
+    if (msglen <= 0)
+    {
+        return LUCRETIA_ERROR_REQUEST_SERIALIZATION;
+    }
+
+    nsend = send(sockfd, (void *)message, msglen, 0);
+    if (nsend < 0)
+    {
+        return LUCRETIA_ERROR_SEND_OPERATION;
+    }
+
+    return nsend;
 }
 
 static enum conf_type set_conf_type(enum conf_type type)

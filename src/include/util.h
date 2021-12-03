18a4e622272c3cb27d75999890d70b55913714b9
clfv5 <bedirhanakcay@icloud.com>
2021-12-03 09:57:29 +0300

    helper function for generating uuid added

--- src/include/util.h
+++ src/include/util.h
@@ -3,6 +3,9 @@
 
 #include "lucretia.h"
 
+#define UTIL_ERROR_INSUFFICIENT_BUFFER_LEN -1
+
 enum conf_type get_conf_type(char *property);
+int get_uuid(char *buff, int len);
 
 #endif // !__UTIL_C_
--- src/util.c
+++ src/util.c
@@ -1,18 +1,19 @@
 #include "include/util.h"
 
 #include <string.h>
+#include <uuid/uuid.h>
 
 enum conf_type get_conf_type(char *property)
 {
-    if(property == NULL)
+    if (property == NULL)
     {
         return UNKNOWN;
     }
-    else if(strcasecmp(property, "MASTER") == 0)
+    else if (strcasecmp(property, "MASTER") == 0)
     {
         return MASTER;
     }
-    else if(strcasecmp(property, "SLAVE") == 0)
+    else if (strcasecmp(property, "SLAVE") == 0)
     {
         return SLAVE;
     }
@@ -20,4 +21,18 @@ enum conf_type get_conf_type(char *property)
     {
         return UNKNOWN;
     }
+}
+
+int get_uuid(char *buff, int len)
+{
+    uuid_t binuuid;
+    uuid_generate_random(binuuid);
+
+    if(len < UUID_STR_LEN){
+        return UTIL_ERROR_INSUFFICIENT_BUFFER_LEN;
+    }
+
+    uuid_unparse_lower(binuuid, buff);
+
+    return 0;
 }

f2bfd567a00032f7dd7ff399bb08b7cc2d938ef6
cflv5 <bedirhanakcay@icloud.com>
2021-12-24 10:18:25 +0300

    feat: implement process message module - serializing and deserializing messages - helper function to send a message to process

---
+++ src/include/process_message.h
@@ -0,0 +1,30 @@
+#ifndef __PROCESS_MESSAGE_H__
+#define __PROCESS_MESSAGE_H__
+
+#define PROCESS_MESSAGE_OK 0
+
+#define PROCESS_MESSAGE_ERROR_NULL_POINTER -1
+#define PROCESS_MESSAGE_ERROR_MEMORY_ALLOCATION -2
+#define PROCESS_MESSAGE_ERROR_SERILIAZATION -3
+#define PROCESS_MESSAGE_ERROR_WRITE_TO_PIPE -4
+#define PROCESS_MESSAGE_ERROR_EMPTY_MESSAGE -5
+
+
+#include "process_message.h"
+#include "m_process.h"
+
+struct process_message
+{
+    char code;
+    char *data;
+};
+
+int serialize_process_message(struct process_message *message, char *buff, int len);
+struct process_message *deserialize_process_message(char *message, int len, int *status);
+
+int send_process_message(struct process_message *message, struct m_process *ps);
+
+int free_process_message(struct process_message **msg);
+
+#endif // !__PROCESS_MESSAGE_H__
+

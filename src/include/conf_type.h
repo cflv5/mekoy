7522558f7d5341a021d9044fd1e623b8ec185eb1
cflv5 <bedirhanakcay@icloud.com>
2022-01-08 11:52:51 +0300

    feat: add conf_type header file

---
+++ src/include/conf_type.h
@@ -0,0 +1,14 @@
+#ifndef __CONF_TYPE__H_
+#define __CONF_TYPE__H_
+
+/*
+ * Configuration types of a Lucretia server.
+ */
+enum conf_type
+{
+    MASTER,
+    SLAVE,
+    UNKNOWN
+};
+
+#endif // !__CONF_TYPE__H_

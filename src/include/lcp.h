c3e0d0a3486bd7237662d45d51cd66ceb7c9f5c6
cflv5 <bedirhanakcay@icloud.com>
2022-01-08 12:02:14 +0300

    feat: move lcp requst functions to lcp src

--- src/include/lcp.h
+++ src/include/lcp.h
@@ -2,8 +2,11 @@
 #define __LCP_H_
 
 #include <sys/types.h>
+#include <netinet/in.h>
 
 #define LCP_ERROR_NULL_POINTER -1
+#define LCP_ERROR_REQUEST_SERIALIZATION -2
+#define LCP_ERROR_SEND_OPERATION -3
 
 #define L_OP_HANDSHAKE 1
 #define L_OP_UNSUPPORTED_OPERATION 99
@@ -25,5 +28,14 @@ struct lcp_req *deserialize_lcp_req(const char *message, int msize, int *size);
 int clear_lcp_req(struct lcp_req *req);
 int destroy_lcp_req(struct lcp_req *req);
 int to_str_lcp_req(struct lcp_req *req, char *buff, int len);
+
+int send_lcp_request(int sockfd, struct lcp_req *req);
+int send_lcp_request_to_addr(struct sockaddr_in *addr, struct lcp_req *req);
+int populate_lcp_request(struct lcp_req *req, u_char ver, const char *id,
+                const char *msgid, u_int16_t opcode, const char *header, 
+                const char *body);
+struct lcp_req *create_lcp_request(u_char ver, const char *id, const char *msgid, 
+                                    u_int16_t opcode, const char *header, 
+                                    const char *body);               
 
 #endif // !__LCP_H_

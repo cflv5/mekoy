feb1c534e9d65c19586cce8001549c89da2495ef
cflv5 <bedirhanakcay@icloud.com>
2022-04-03 12:47:44 +0000

    fix: refactor and use send correct code to child

--- src/include/lcp_handler.h
+++ src/include/lcp_handler.h
@@ -7,5 +7,6 @@
 #include <netinet/in.h>
 
 int handle_lcp_informed(struct lucretia *server, int sockfd, struct sockaddr_in req_addr, struct lcp_req *original_req);
+int handle_lcp_informed_clear(struct lucretia *server, int sockfd, struct sockaddr_in req_addr, struct lcp_req *original_req);
 
 #endif // !__LCP_HANDLER__H
--- src/lcp_handler.c
+++ src/lcp_handler.c
@@ -7,7 +7,7 @@
 #include "include/m_process.h"
 #include "include/map.h"
 
-static int send_message_to_pfd(struct m_process *ps, char code, char *message);
+static int send_message_to_pfd_as_child(struct m_process *ps, char code, char *message);
 
 int handle_lcp_informed(struct lucretia *server, int sockfd, struct sockaddr_in req_addr, struct lcp_req *original_req)
 {
@@ -18,29 +18,40 @@ int handle_lcp_informed(struct lucretia *server, int sockfd, struct sockaddr_in 
 
     char *anomaly_msg = original_req->body;
     
-    struct m_process *pr = (struct m_process *)mapGet(MEKOY_PROCESS_AID_CAR_CONTROL, server->ccf->ps);
+    struct m_process *pr = (struct m_process *)mapGet(MEKOY_PROCESS_CV, server->ccf->ps);
 
     if (pr != NULL)
     {
-        send_message_to_pfd(pr, 'S', NULL);
-    }
-
-    pr = (struct m_process *)mapGet(MEKOY_PROCESS_ROAD_INFORM_CONTROL, server->ccf->ps);
-
-    if (pr != NULL)
-    {
-        send_message_to_pfd(pr, 'I', anomaly_msg);
+        send_message_to_pfd_as_child(pr, 'D', anomaly_msg);
     }
 
     return 0;
 }
 
-static int send_message_to_pfd(struct m_process *ps, char code, char *message)
+int handle_lcp_informed_clear(struct lucretia *server, int sockfd, struct sockaddr_in req_addr, struct lcp_req *original_req)
+{
+    if (server->type != MASTER)
+    {
+        return 1; // NON SLAVE OP
+    }
+
+    struct m_process *pr = (struct m_process *)mapGet(MEKOY_PROCESS_CV, server->ccf->ps);
+
+    if (pr != NULL)
+    {
+        send_message_to_pfd_as_child(pr, 'C', NULL);
+    }
+
+    return 0;
+}
+
+
+static int send_message_to_pfd_as_child(struct m_process *ps, char code, char *message)
 {
     struct process_message msg;
 
     msg.code = code;
     msg.data = message;
 
-    return send_process_message(&msg, ps);
+    return send_process_message_as_child(&msg, ps);
 }

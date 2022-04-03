5bad34490b9d8e42720ee09fa98bf7c50211b76a
cflv5 <bedirhanakcay@icloud.com>
2022-04-03 11:07:06 +0300

    feat: implement list and its iterator functions

--- lib/list/list.c
+++ lib/list/list.c
@@ -1,9 +1,9 @@
 /**
  * @author: Bedirhan AKÇAY
  * @date: 18.12.2021
- * 
+ *
  * Link list implementation
-*/
+ */
 
 #include "list.h"
 
@@ -21,10 +21,17 @@ struct list
     int len;
 };
 
+struct list_iterator
+{
+    struct list *list;
+    int ind;
+    struct node *node;
+};
+
 struct list *create_list()
 {
-    struct list *list = (struct list*)malloc(sizeof(struct list));
-    if(list == NULL)
+    struct list *list = (struct list *)malloc(sizeof(struct list));
+    if (list == NULL)
     {
         return NULL;
     }
@@ -39,37 +46,114 @@ int insert_list(struct list *list, void *data)
 {
     struct node *tmp;
     struct node *node;
-    int i;
 
-    if(list == NULL)
+    if (list == NULL)
     {
         return LIST_ERROR_NULL_POINTER;
     }
 
-    node = (struct node*)malloc(sizeof(struct node));
+    node = (struct node *)malloc(sizeof(struct node));
     if (node == NULL)
     {
         return LIST_ERROR_MEMMORY_ALLOCATION;
     }
     node->data = data;
     node->next = NULL;
-    
+
     tmp = list->data;
-    for (i = 0; i < list->len; i++)
-    {
-        tmp = node->next;
-    }
-    
     if (tmp == NULL)
     {
         list->data = node;
     }
     else
     {
+        while (tmp->next != NULL)
+        {
+            tmp = tmp->next;
+        }
+
         tmp->next = node;
     }
-    
+
     list->len++;
 
     return LIST_CODE_OK;
+}
+
+void *list_get_node_data(struct node *node)
+{
+    void *data = NULL;
+    if (node != NULL)
+    {
+        data = node->data;
+    }
+    return data;
+}
+
+struct list_iterator *create_list_iterator(struct list *list)
+{
+    struct list_iterator *iterator = NULL;
+
+    if (list == NULL)
+    {
+        return NULL;
+    }
+
+    iterator = (struct list_iterator *)malloc(sizeof(struct list_iterator));
+    if (iterator == NULL)
+    {
+        return NULL;
+    }
+
+    iterator->list = list;
+    iterator->ind = -1;
+    iterator->node = NULL;
+
+    return iterator;
+}
+
+int list_iterator_next(struct list_iterator *iterator)
+{
+    struct list *list;
+    if (iterator == NULL)
+    {
+        return 1;
+    }
+
+    list = iterator->list;
+    if (list == NULL)
+    {
+        return LIST_ITERATOR_ERROR_LIST_NOT_DEFINED;
+    }
+
+    if (list->len == 0 || (iterator->ind + 1) >= list->len)
+    {
+        return LIST_ITERATOR_ERROR_INDEX_OUT_OF_BOUNDRY;
+    }
+    else
+    {
+        if (iterator->ind == -1)
+        {
+            iterator->node = list->data;
+        }
+        else
+        {
+            iterator->node = iterator->node->next;
+        }
+        iterator->ind++;
+    }
+
+    return 0;
+}
+
+struct node *list_iterator_get_node(struct list_iterator *iterator)
+{
+    void *node = NULL;
+
+    if (iterator != NULL && iterator->node != NULL)
+    {
+        node = iterator->node;
+    }
+
+    return node;
 }

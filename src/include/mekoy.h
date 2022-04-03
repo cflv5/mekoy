2b8e985ec4d509ab09a8b40a5c9cee2026228ef4
cflv5 <bedirhanakcay@icloud.com>
2022-04-03 12:51:58 +0000

    feat: add puppeteer process when mekoy initialized

--- src/include/mekoy.h
+++ src/include/mekoy.h
@@ -2,12 +2,14 @@
 #define __MEKOY_H__
 
 struct lucretia;
+struct puppeteer;
 
 #include "map.h"
 #include "lucretia.h"
 #include "process_listener.h"
 #include "m_process.h"
 #include "conf_type.h"
+#include "puppeteer.h"
 
 #include <pthread.h>
 
@@ -25,6 +27,11 @@ struct lucretia;
 #define MEKOY_ERROR_PROCESS_CREATION -6
 #define MEKOY_ERROR_MUTEX_INIT -7
 #define MEKOY_ERROR_FETCH_PROCESS -8
+#define MEKOY_ERROR_REMOTE_CONTROL_CONFIGURATION_NOT_DEFINED -9
+#define MEKOY_ERROR_REMOTE_CONTROL_SERVER_CREATION -10
+#define MEKOY_ERROR_REMOTE_CONTROL_CREATION -11
+#define MEKOY_ERROR_REMOTE_CONTROL_NULL_POINTER -12
+#define MEKOY_ERROR_REMOTE_CONTROL_PROCESS_NOT_CREATED -13
 
 
 #define MEKOY_SIZE_PROCESS 4
@@ -34,6 +41,7 @@ struct mekoy
     struct map* configurations;
     struct map* ps;
     struct lucretia *lucretia;
+    struct puppeteer *puppeteer;
 
     enum conf_type type;
 };
